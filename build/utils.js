acc3dd2a4a8d12a87ac0ad96b18fe2b77827b821
Emily Curry <emily.curry@artisantechgroup.com>
2019-06-28 17:53:36 -0500

    Include build files

---
+++ build/utils.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isObject(obj) {
+    return Object.prototype.toString.call(obj) === "[object Object]";
+}
+exports.isObject = isObject;
